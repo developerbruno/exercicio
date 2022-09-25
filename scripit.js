cb79dae0337908e26c7fecd2f92ddeb2d9bc8448
bruno rangel <bruno.developer021@gmail.com>
2022-09-25 09:39:01 -0300

    dever

---
+++ scripit.js
@@ -0,0 +1,17 @@
+let x = 10;
+let y = 5;
+
+console.log(x > y)
+
+let w = "10";
+let z = 10;
+
+console.log(w === z)
+
+let preco = 40.3
+
+if (preco< 45){
+    console.log("barato")
+
+} else { console.log("caro")
+}
